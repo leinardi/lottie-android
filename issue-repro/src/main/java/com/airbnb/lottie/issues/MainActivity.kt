ddac7cb033548369ebcec7b435e80af874e02fb7
Roberto Leinardi <leinardi@gmail.com>
2020-03-07 20:19:07 +0100

    LottieDrawable not displaying correctly on a MaterialButton

--- issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.airbnb.lottie.issues
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityOptionsCompat
-import com.airbnb.lottie.issues.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.airbnb.lottie.LottieCompositionFactory
+import com.airbnb.lottie.LottieDrawable
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Reproduce any issues here.
+
+        val lottieDrawable = LottieDrawable()
+
+        LottieCompositionFactory.fromRawRes(this, R.raw.heart).addListener { lottieComposition ->
+            lottieDrawable.composition = lottieComposition
+            lottieDrawable.repeatCount = LottieDrawable.INFINITE
+            lottieDrawable.scale = 0.5f
+            lottieDrawable.playAnimation()
+        }
+
+
+        findViewById<MaterialButton>(R.id.button).apply {
+            icon = lottieDrawable
+        }
     }
 }
